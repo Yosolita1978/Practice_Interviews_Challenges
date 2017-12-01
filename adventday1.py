55cb2ec4aca9470e0fd7772308cc79e4f0c83577
Cristina Rodriguez <cristina@Cristinas-MacBook-Air.local>
2017-12-01 12:33:38 -0800

    Part two

--- adventday1.py
+++ adventday1.py
@@ -1,4 +1,5 @@
 list_nums = []
+list_nums_part2 = []
 
 
 with open("input_day1.txt", "r") as inputfile:
@@ -7,6 +8,10 @@ with open("input_day1.txt", "r") as inputfile:
 for i in range(0, len(string)):
     if string[i] == string[i-1]:
         list_nums.append(int(string[i]))
+    if string[i] == string[i-(len(string)/2)]:
+        list_nums_part2.append(int(string[i]))
 
 print(list_nums)
 print(sum(list_nums))
+print(list_nums_part2)
+print(sum(list_nums_part2))
